ecd01472d0a02f206328e0b1085bcecd4a02e9f7
Querz <admin@querz.net>
2018-11-23 15:37:56 +0100

    improve bits ChessLoader

--- src/main/java/net/querz/chess/io/bits/ChessLoader.java
+++ src/main/java/net/querz/chess/io/bits/ChessLoader.java
@@ -13,12 +13,12 @@ import java.util.List;
 public class ChessLoader implements ChessIO {
 
 	//current turn: 16 bits unsigned short
-	//50-move-rule: 16 bits unsigned short
+	//50-move-rule: 7 bits unsigned byte
 	//number of black figures: 6 bits unsigned byte
-	//first turn: 16 bits unsigned short
+	//type: 3 bits unsigned byte
+	//first turn: 16 bits unsigned short (only for pawns, rooks and kings)
 	//pos-x: 3 bits unsigned byte
 	//pos-y: 3 bits unsigned byte
-	//type: 3 bits unsigned byte
 	//...
 	//number of white figures: 6 bits unsigned byte
 	//...
@@ -28,7 +28,7 @@ public class ChessLoader implements ChessIO {
 		ByteArrayInputStream bais = new ByteArrayInputStream(data);
 		try (BitReader br = new BitReader(bais)) {
 			board.setCurrenTurn(br.readUnsignedShort());
-			board.set50MoveRuleTurns(br.readUnsignedShort());
+			board.set50MoveRuleTurns(br.readBits(7));
 			loadFigures(Color.BLACK, br, board);
 			loadFigures(Color.WHITE, br, board);
 		} catch (IOException ex) {
@@ -39,10 +39,13 @@ public class ChessLoader implements ChessIO {
 	private void loadFigures(Color color, BitReader br, ChessBoard board) throws IOException {
 		int i = br.readBits(6);
 		for (int j = 0; j < i; j++) {
-			int firstTurn = (int) br.readBits(8) << 8 | (int) br.readBits(8);
+			int id = br.readBits(3);
+			int firstTurn = -1;
+			if (id == 0 || id == 1 || id == 5) {
+				firstTurn = (int) br.readBits(8) << 8 | (int) br.readBits(8);
+			}
 			int x = br.readBits(3);
 			int y = br.readBits(3);
-			int id = br.readBits(3);
 			Figure figure = FigureFactory.createFigure(color, id, x, y, firstTurn);
 			board.setFigure(figure);
 		}
@@ -53,7 +56,8 @@ public class ChessLoader implements ChessIO {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		try (BitWriter bw = new BitWriter(baos)) {
 			bw.writeShort(board.getCurrentTurn());
-			bw.writeShort(board.get50MoveRuleTurns());
+			int fifty = board.get50MoveRuleTurns();
+			bw.writeBits((byte) (fifty > 100 ? 100 : fifty), 7);
 			writeFigures(board.getFigures(Color.BLACK), bw);
 			writeFigures(board.getFigures(Color.WHITE), bw);
 		} catch (IOException ex) {
@@ -65,12 +69,15 @@ public class ChessLoader implements ChessIO {
 	private void writeFigures(List<Figure> figures, BitWriter bw) throws IOException {
 		bw.writeBits((byte) figures.size(), 6);
 		for (Figure f : figures) {
-			int firstTurn = f.getFirstTurn();
-			bw.writeBits((byte) (firstTurn >>> 8), 8);
-			bw.writeBits((byte) (firstTurn & 0xFF), 8);
+			int id = FigureFactory.getFigureID(f);
+			bw.writeBits((byte) id, 3);
+			if (id == 0 || id == 1 || id == 5) {
+				int firstTurn = f.getFirstTurn();
+				bw.writeBits((byte) (firstTurn >>> 8), 8);
+				bw.writeBits((byte) (firstTurn & 0xFF), 8);
+			}
 			bw.writeBits((byte) f.getX(), 3);
 			bw.writeBits((byte) f.getY(), 3);
-			bw.writeBits((byte) FigureFactory.getFigureID(f), 3);
 		}
 	}
 
